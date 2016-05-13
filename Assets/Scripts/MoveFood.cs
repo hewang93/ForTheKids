9124dc4f1dfb570bf4fda6c5728ac7d6bdc79949
Chris Kim <chris.kim342@gmail.com>
2016-05-13 02:18:12 -0700

    baseline

---
+++ Assets/Scripts/MoveFood.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFood : MonoBehaviour {
+	private float moveSpeed = 0.08f;
+	private Vector2 first;
+	private Vector2 second;
+	private Vector2 swipe;
+	private bool wasPressed;
+
+	// Use this for initialization
+	void Start () {
+		wasPressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (transform.position.x - moveSpeed, transform.position.y, transform.position.z);
+
+		// kill it once off screen
+		if (transform.position.x < -10f)
+			Destroy (this.gameObject);
+	}
+
+	void OnMouseOver() {
+		if (Input.GetMouseButtonDown (0)) {
+			first = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+			wasPressed = true;
+		} 
+	}
+
+	void OnMouseExit() {
+		if (wasPressed) {
+			wasPressed = false;
+			second = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+			swipe = new Vector2 (second.x - first.x, second.y - first.y);
+			swipe.Normalize ();
+			if (swipe.y < 0 && swipe.x > -.4f && swipe.x < .4f)
+				Destroy (this.gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/SpawnFood.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnFood : MonoBehaviour {
+	private static int NUM_OF_FOOD_TYPES = 2;
+	private float time = 3f;
+
+	public Transform foodTemp;
+
+	// Use this for initialization
+	void Start () {
+		Instantiate (foodTemp);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time -= Time.deltaTime;
+
+		if (time <= 0) {
+			time = 3f;
+			Instantiate (foodTemp);
+		}
+	}
+}
